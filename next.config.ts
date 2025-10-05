377d5819c51a553332ea14ca4aeb3ef08356eaf1
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-10-05 21:20:22 +0700

    feat: update dependencies and integrate internationalization
    - Added @prisma/client and prisma for database management.
    - Integrated next-intl for internationalization support.
    - Updated layout to support dynamic locale and messages.
    - Created custom hook useLanguage for managing language state.
    - Added English and Chinese language JSON files for translations.
    - Removed unused SVG files and favicon.
    - Created Prisma schema for PostgreSQL datasource.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin("./src/config/request.ts");
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
---
+++ src/hooks/useLanguage.ts
@@ -0,0 +1,34 @@
+import { Language } from "@/types/language";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const useLanguage = () => {
+  const [locale, setLocale] = useState<string>("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const cookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("locale="))
+      ?.split("=")[1];
+
+    if (cookie) {
+      setLocale(cookie);
+    } else {
+      const defaultLocale = navigator.language.slice(0, 2);
+      setLocale(defaultLocale);
+      document.cookie = `locale=${defaultLocale}; path=/`;
+      router.refresh();
+    }
+  }, [router]);
+
+  const changeLanguage = (newLocale: Language) => {
+    document.cookie = `locale=${newLocale}; path=/`;
+    setLocale(newLocale);
+    router.refresh();
+  };
+
+  return { locale, changeLanguage };
+};
+
+export default useLanguage;
