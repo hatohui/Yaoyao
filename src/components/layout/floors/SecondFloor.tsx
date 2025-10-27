8f3816700f50d02e2adcfa782b621425d4004da2
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-10-27 15:26:43 +0700

    feat: refactor FirstFloor and SecondFloor layouts for improved structure and readability

--- src/components/layout/floors/SecondFloor.tsx
+++ src/components/layout/floors/SecondFloor.tsx
@@ -6,43 +6,38 @@ const SecondFloor = () => {
   const t = useTranslations("layout");
 
   return (
-    <section className="absolute grid grid-cols-[1fr_4fr] grid-rows-1 h-full w-full">
-      <div className="grid grid-rows-3 border-r-2 border-[#94A3B8]">
-        <Area full noLeft noTop noRight label={t("fishTank")} />
-        <div className="grid grid-cols-4">
-          <div className="col-span-3" />
-          <div className="grid grid-rows-3">
-            <div />
-            <Area full noBottom noRight />
-            <Area full noTop noRight />
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-5">
+    <section className="absolute grid grid-cols-[1fr_4fr] grid-rows-3 h-full w-full">
+      <Area full noBottom noRight noTop noLeft />
+      <Area full noBottom noRight noTop />
+      <Area full noTop noBottom noRight noLeft />
+      <div className="grid grid-cols-3">
+        <Area full noTop noBottom noRight />
         <div />
-        <div className="grid col-span-2 ">
-          <div className="grid grid-rows-6">
-            <div className="row-span-4" />
-            <div className="grid grid-cols-8">
+        <div className="grid grid-cols-2">
+          <div className="grid grid-rows-[4fr_1fr]">
+            <Area full label={t("bar")} />
+            <div className="grid grid-cols-3">
               <div />
-              <Area full />
-            </div>
-          </div>
-          <div className="grid grid-rows-4">
-            <div className="row-span-3" />
-            <Area full noBottom />
-          </div>
-        </div>
-        <div className="grid grid-rows-2 col-span-2">
-          <Area full label={t("kitchen")} noTop />
-          <div className="grid grid-rows-[3fr_2fr]">
-            <div className="grid grid-cols-5">
-              <div className="col-span-3" />
+              <div />
               <Area full noTop />
             </div>
           </div>
+          <Area full noLeft />
         </div>
       </div>
+      <Area full noTop noRight noLeft />
+      <div className="grid grid-cols-3">
+        <Area full noTop noRight />
+        <div />
+        <div className="grid grid-rows-[1fr_3fr]">
+          <div />
+          <div className="grid grid-cols-[1fr_2fr]">
+            <div />
+            <Area full label={t("v4")} />
+          </div>
+        </div>
+      </div>
+      <Area full noBottom />
     </section>
   );
 };
